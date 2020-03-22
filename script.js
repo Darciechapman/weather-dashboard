bf11761ff27ab5bcc8336fecdcf5820b8d52aeed
Darciechapman <darciechapman96@gmail.com>
2020-03-22 18:52:09 +0800

    adding current and forecast information

---
+++ script.js
@@ -0,0 +1,73 @@
+var date = new Date();
+var dayDate = date.getDate();
+var monthDate = date.getMonth() + 1;
+var yearDate = date.getFullYear();
+
+var APIKey = "api.openweathermap.org/data/2.5/weather?q=perth&appid=d6db38001e351111dd620023b7c30d07";
+var currentQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=perth&units=metric&appid=d6db38001e351111dd620023b7c30d07";
+var forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=perth&units=metric&appid=d6db38001e351111dd620023b7c30d07"
+
+$.ajax({
+  url: currentQueryURL,
+  method: "GET"
+}).then(function(response) {
+
+    function addCurrentWeather() {
+
+        var currentWeatherContainer = $("<div>");  
+        var titleId = $("<h3>").text(response.name + " " + dayDate + "/" + monthDate + "/" + yearDate);
+        var tempId = $("<h5>").text("Temperature: " + response.main.temp);
+        var windId = $("<h5>").text("Wind speed: " + response.wind.speed);
+        var humidityId = $("<h5>").text("Humidity: " + response.main.humidity);
+  
+        titleId.text(response.name + " " + dayDate + "/" + monthDate + "/" + yearDate);
+  
+        currentWeatherContainer.append(titleId);
+        currentWeatherContainer.append(tempId);
+        currentWeatherContainer.append(windId);
+        currentWeatherContainer.append(humidityId);
+      
+  
+        $("#current-weather").append(currentWeatherContainer);
+  
+    }
+    
+addCurrentWeather();
+
+})
+
+$.ajax({
+    url: forecastQueryURL,
+    method: "GET"
+}).then(function(response) {
+
+    
+
+      console.log(response);
+      console.log(response.list[1].dt_txt)
+
+    var list5thDay = [7, 15, 23, 31, 39]
+
+    list5thDay.forEach(function(e) {
+
+        var forecastContainer = $("<div>").addClass("forecast-block");  
+        var dateId = $("<h5>").text(response.list[e].dt_txt);
+        var iconId = $("<i>").addClass(response.list[e].weather.icon);
+        var tempId = $("<p>").text("Temp: " + response.list[e].main.temp);
+        var humidityId = $("<p>").text("Humidity: " + response.list[e].main.humidity);
+  
+        dateId.text(response.list[e].dt_txt);
+  
+        forecastContainer.append(dateId);
+        forecastContainer.append(iconId);
+        forecastContainer.append(tempId);
+        forecastContainer.append(humidityId);
+      
+        
+        $("#forecast-block").append(forecastContainer);
+  
+    }) 
+    
+
+
+})
