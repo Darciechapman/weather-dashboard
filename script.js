9c8c81f6acea03e278e54c1557f95f0f5e1631a8
Darciechapman <darciechapman96@gmail.com>
2020-03-23 13:57:12 +0800

    search onclick

--- script.js
+++ script.js
@@ -2,35 +2,62 @@ var date = new Date();
 var dayDate = date.getDate();
 var monthDate = date.getMonth() + 1;
 var yearDate = date.getFullYear();
+var locationEl = $("#search-input").val();
 
-//$("#gsearch").on("keypress", function() {
+$("#search").on("click", function(event) {
 
-    
-    //$("#search").val()
+    event.preventDefault();
 
-    var currentQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=perth&units=metric&appid=d6db38001e351111dd620023b7c30d07";
-    var forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=perth&units=metric&appid=d6db38001e351111dd620023b7c30d07"
+    var locationEl = $("#search-input").val();
+
+    if (locationEl === "") {
+        $("#search-input").removeAttr('placeholder');
+        $("#search-input").attr("placeholder", "Error")
+    } else {
+        
+        var currentQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + locationEl + "&units=metric&appid=d6db38001e351111dd620023b7c30d07";
+        var forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + locationEl + "&units=metric&appid=d6db38001e351111dd620023b7c30d07"
+
+        createLocationBtn();
+    }
+
+    function createLocationBtn() {
+
+            var btnId = $("<button>").text(locationEl);
+            btnId.addClass("btn-list")
+
+            $("#button-list").append(btnId);
+        
+    }
 
     $.ajax({
     url: currentQueryURL,
     method: "GET"
     }).then(function(response) {
 
+        $("#current-weather").empty();
+
         function addCurrentWeather() {
+
+            event.preventDefault();
+
+            var iconNumber = response.weather[0].icon
 
             var currentWeatherContainer = $("<div>");  
             var titleId = $("<h3>").text(response.name + " " + dayDate + "/" + monthDate + "/" + yearDate);
+            var iconId = $("<img>").attr("src","http://openweathermap.org/img/wn/" + iconNumber + "@2x.png");
+            iconId.width("100px");
             var tempId = $("<h5>").text("Temperature: " + response.main.temp + " °C");
             var windId = $("<h5>").text("Wind speed: " + response.wind.speed + " MPH");
             var humidityId = $("<h5>").text("Humidity: " + response.main.humidity + " %");
-  
+        
             titleId.text(response.name + " " + dayDate + "/" + monthDate + "/" + yearDate);
   
             currentWeatherContainer.append(titleId);
+            currentWeatherContainer.append(iconId);
             currentWeatherContainer.append(tempId);
             currentWeatherContainer.append(windId);
             currentWeatherContainer.append(humidityId);
-      
   
             $("#current-weather").append(currentWeatherContainer);
   
@@ -45,15 +72,15 @@ var yearDate = date.getFullYear();
         method: "GET"
     }).then(function(response) {
 
-    
-      console.log(response);
+        event.preventDefault();
       
         var list5thDay = [7, 15, 23, 31, 39]
+
+        $("#forecast-block").empty();
 
         list5thDay.forEach(function(e) {
 
             var iconNumber = response.list[e].weather[0].icon
-            console.log(iconNumber)
 
             var forecastContainer = $("<div>").addClass("forecast-block");  
             var dateId = $("<h5>").text(response.list[e].dt_txt);
@@ -75,8 +102,6 @@ var yearDate = date.getFullYear();
   
         }) 
     
-
-
     })
 
-//})
+})
