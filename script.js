2b2eb09e1a1b4c7e1b50f204c80e9978d791c2ce
Darciechapman <darciechapman96@gmail.com>
2020-03-23 11:51:52 +0800

    icons now working for forecast

--- script.js
+++ script.js
@@ -3,71 +3,80 @@ var dayDate = date.getDate();
 var monthDate = date.getMonth() + 1;
 var yearDate = date.getFullYear();
 
-var APIKey = "api.openweathermap.org/data/2.5/weather?q=perth&appid=d6db38001e351111dd620023b7c30d07";
-var currentQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=perth&units=metric&appid=d6db38001e351111dd620023b7c30d07";
-var forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=perth&units=metric&appid=d6db38001e351111dd620023b7c30d07"
+//$("#gsearch").on("keypress", function() {
 
-$.ajax({
-  url: currentQueryURL,
-  method: "GET"
-}).then(function(response) {
+    
+    //$("#search").val()
 
-    function addCurrentWeather() {
+    var currentQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=perth&units=metric&appid=d6db38001e351111dd620023b7c30d07";
+    var forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=perth&units=metric&appid=d6db38001e351111dd620023b7c30d07"
 
-        var currentWeatherContainer = $("<div>");  
-        var titleId = $("<h3>").text(response.name + " " + dayDate + "/" + monthDate + "/" + yearDate);
-        var tempId = $("<h5>").text("Temperature: " + response.main.temp);
-        var windId = $("<h5>").text("Wind speed: " + response.wind.speed);
-        var humidityId = $("<h5>").text("Humidity: " + response.main.humidity);
+    $.ajax({
+    url: currentQueryURL,
+    method: "GET"
+    }).then(function(response) {
+
+        function addCurrentWeather() {
+
+            var currentWeatherContainer = $("<div>");  
+            var titleId = $("<h3>").text(response.name + " " + dayDate + "/" + monthDate + "/" + yearDate);
+            var tempId = $("<h5>").text("Temperature: " + response.main.temp + " °C");
+            var windId = $("<h5>").text("Wind speed: " + response.wind.speed + " MPH");
+            var humidityId = $("<h5>").text("Humidity: " + response.main.humidity + " %");
   
-        titleId.text(response.name + " " + dayDate + "/" + monthDate + "/" + yearDate);
+            titleId.text(response.name + " " + dayDate + "/" + monthDate + "/" + yearDate);
   
-        currentWeatherContainer.append(titleId);
-        currentWeatherContainer.append(tempId);
-        currentWeatherContainer.append(windId);
-        currentWeatherContainer.append(humidityId);
+            currentWeatherContainer.append(titleId);
+            currentWeatherContainer.append(tempId);
+            currentWeatherContainer.append(windId);
+            currentWeatherContainer.append(humidityId);
       
   
-        $("#current-weather").append(currentWeatherContainer);
+            $("#current-weather").append(currentWeatherContainer);
   
-    }
+        }
     
-addCurrentWeather();
+    addCurrentWeather();
 
-})
+    })
 
-$.ajax({
-    url: forecastQueryURL,
-    method: "GET"
-}).then(function(response) {
+    $.ajax({
+        url: forecastQueryURL,
+        method: "GET"
+    }).then(function(response) {
 
     
-
       console.log(response);
-      console.log(response.list[1].dt_txt)
+      
+        var list5thDay = [7, 15, 23, 31, 39]
 
-    var list5thDay = [7, 15, 23, 31, 39]
+        list5thDay.forEach(function(e) {
 
-    list5thDay.forEach(function(e) {
+            var iconNumber = response.list[e].weather[0].icon
+            console.log(iconNumber)
 
-        var forecastContainer = $("<div>").addClass("forecast-block");  
-        var dateId = $("<h5>").text(response.list[e].dt_txt);
-        var iconId = $("<i>").addClass(response.list[e].weather.icon);
-        var tempId = $("<p>").text("Temp: " + response.list[e].main.temp);
-        var humidityId = $("<p>").text("Humidity: " + response.list[e].main.humidity);
+            var forecastContainer = $("<div>").addClass("forecast-block");  
+            var dateId = $("<h5>").text(response.list[e].dt_txt);
+            var iconId = $("<img>").attr("src","http://openweathermap.org/img/wn/" + iconNumber + "@2x.png");
+            iconId.width("35px")
+
+            var tempId = $("<p>").text("Temp: " + response.list[e].main.temp + " °C");
+            var humidityId = $("<p>").text("Humidity: " + response.list[e].main.humidity + " %");
   
-        dateId.text(response.list[e].dt_txt);
+            dateId.text(response.list[e].dt_txt);
   
-        forecastContainer.append(dateId);
-        forecastContainer.append(iconId);
-        forecastContainer.append(tempId);
-        forecastContainer.append(humidityId);
+            forecastContainer.append(dateId);
+            forecastContainer.append(iconId);
+            forecastContainer.append(tempId);
+            forecastContainer.append(humidityId);
       
         
-        $("#forecast-block").append(forecastContainer);
+            $("#forecast-block").append(forecastContainer);
   
-    }) 
+        }) 
     
 
 
-})
+    })
+
+//})
